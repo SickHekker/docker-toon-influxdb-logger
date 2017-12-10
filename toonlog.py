b8a4466951b3d1579177b8289a00881b31a70058
SickHekker <Robindehek@gmail.com>
2017-12-10 19:11:28 +0100

    fix ident

--- toonlog.py
+++ toonlog.py
@@ -17,30 +17,30 @@ def save_data(sensor):
 	toon.login()
    	current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     	thermostat = toon.get_thermostat_info()
-	temperature = temp = float(thermostat["currentTemp"]) / 100"
+	temperature = temp = float(thermostat["currentTemp"]) / 100
 	toon.logout()
-    if temperature is not None:
-        print('Sensor={0}  Temp={1:0.1f}*C'.format(sensor, temperature))
-        json_body = [
-            {
-                "measurement": "{0}_temperature".format(sensor),
-                "tags": {
-                    "celsius": "temperature"
-                },
-                "timestamp": current_time,
-                "fields": {
-                    "value": float("{0:0.1f}".format(temperature))
-                }
-            }]
+	if temperature is not None:
+        	print('Sensor={0}  Temp={1:0.1f}*C'.format(sensor, temperature))
+        	json_body = [
+            	{
+                	"measurement": "{0}_temperature".format(sensor),
+                	"tags": {
+                    	"celsius": "temperature"
+                	},
+                	"timestamp": current_time,
+                	"fields": {
+                    	"value": float("{0:0.1f}".format(temperature))
+                	}
+           	 }]
         # print(json_body)
-        client.write_points(json_body)
+        	client.write_points(json_body)
 		
 		
 
 while True:
     try:
-		save_data('toon')
-		time.sleep(30) 
+	save_data('toon')
+	time.sleep(30) 
 	except KeyboardInterrupt:
    	sys.exit()
         
