2717f56f89850f7d6fe4abc15ef4bd2a0a75c155
SickHekker <Robindehek@gmail.com>
2018-01-10 14:24:12 +0100

    Update toonlog.py

--- toonlog.py
+++ toonlog.py
@@ -16,7 +16,7 @@ client = InfluxDBClient('address', port, 'user', 'password', 'database')
 def save_data(sensor):
 	toon.login()
    	current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    	thermostat = toon.get_thermostat_info()
+	thermostat = toon.get_thermostat_info()
 	temperature = temp = float(thermostat["currentTemp"]) / 100
 	power = toon.get_power_usage()
 	toon.logout()
