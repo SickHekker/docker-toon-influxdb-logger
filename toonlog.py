0bb7f1fbda097bc7b25df6cabe27c767b0113bc6
SickHekker <Robindehek@gmail.com>
2018-01-10 14:28:54 +0100

    Update toonlog.py

--- toonlog.py
+++ toonlog.py
@@ -13,33 +13,32 @@ toon = Toon(toonusername, toonpassword)
 
 client = InfluxDBClient('address', port, 'user', 'password', 'database')
 
+
 def save_data(sensor):
-	toon.login()
-   	current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-	thermostat = toon.get_thermostat_info()
-	temperature = temp = float(thermostat["currentTemp"]) / 100
-	power = toon.get_power_usage()
-	toon.logout()
-	if temperature is not None:
-        	print('Sensor={0}  Temp={1:0.1f}*C Power={0:0.1f}watts'.format(sensor, temperature, power))
-        	json_body = [
-            	{
-                	"measurement": "toon",
-                	"timestamp": current_time,
-                	"fields": {
-				"temperature": float("{0:0.1f}".format(temperature))
-				"power": power["value"]
-			}
-           	 }]
-        	# print(json_body) #for debug, not enabled
-        	client.write_points(json_body)
-		
-		
+    toon.login()
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    thermostat = toon.get_thermostat_info()
+    temperature = temp = float(thermostat["currentTemp"]) / 100
+    power = toon.get_power_usage()
+    toon.logout()
+    if temperature is not None:
+            print('Sensor={0}  Temp={1:0.1f}*C Power={0:0.1f}watts'.format(sensor, temperature, power))
+            json_body = [
+                {
+                    "measurement": "toon",
+                    "timestamp": current_time,
+                    "fields": {
+                        "temperature": float("{0:0.1f}".format(temperature)),
+                        "power": power["value"]
+                    }
+                }
+            ]
+            # print(json_body) #for debug, not enabled
+            client.write_points(json_body)
 
 while True:
-	try:
-		save_data('toon')
-		time.sleep(30) 
-	except KeyboardInterrupt:
-   		sys.exit()
-        
+    try:
+        save_data('toon')
+        time.sleep(30)
+    except KeyboardInterrupt:
+        sys.exit()
